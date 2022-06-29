b82827b00b85d049ee6945920b20f2a46356f306
Mark Phillips <cafecartelsystems@gmail.com>
2022-06-28 21:18:17 -0700

    Matnifest Not Working

---
+++ android/app/src/main/java/com/dsiEMVAndroid/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.dsiEMVAndroid.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Capacitor} from '@capacitor/core';
+import { dsiemvandroid } from 'dsiemvandroid';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DSIEMVAngular';
+  message : any
+
+  get isAndroid() {
+    const platForm =   Capacitor.getPlatform();
+    if (platForm === 'android') {
+      return true
+    }
+    return false;
+  }
+
+  constructor(   ){
+
+  }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.message = "...waiting for test results."
+  }
+
+  async dsiEMVTest() {
+    try {
+      const options = { value: ' value.'}
+      const item = await dsiemvandroid.echo(options)
+      console.log('this return ', item)
+      this.message = item;
+    } catch (error) {
+      this.message = error;
+    }
+  }
 }
